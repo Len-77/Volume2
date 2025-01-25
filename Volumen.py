155e0f501d4346fe2e7c55eb5ba22b3f556c53a9
Len-77 <lennartschefe@web.de>
2025-01-25 20:27:24 +0100

    Debuging...

--- Volumen.py
+++ Volumen.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from math import pi
-import pyperclip
+#import pyperclip
 
 st.title("Volumenberechnung")
 #st.write("Ein Online-Minitool von Lennart Schefe")
@@ -15,9 +15,9 @@ volume = (4/3)*pi*(x/2)*(y/2)*(z/2)
 st.subheader("Volumen:")
 st.subheader(volume)
 
-if st.button("Kopieren",0):
-    pyperclip.copy(volume)
-    st.write("Das Volumen wurde in die Zwischenablage kopiert.")
+#if st.button("Kopieren",0):
+#    pyperclip.copy(volume)
+#    st.write("Das Volumen wurde in die Zwischenablage kopiert.")
 
 my_digits = st.slider("Runden auf:",0,10,3)
 rounded_volume = round(volume,my_digits)
@@ -26,6 +26,6 @@ st.write("Stellen")
 st.subheader("Gerundetes Volumen:")
 st.subheader(rounded_volume)
 
-if st.button("Kopieren",1):
-    pyperclip.copy(rounded_volume)
-    st.write("Das gerundete Volumen wurde in die Zwischenablage kopiert.")
+#if st.button("Kopieren",1):
+#    pyperclip.copy(rounded_volume)
+#    st.write("Das gerundete Volumen wurde in die Zwischenablage kopiert.")
