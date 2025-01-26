2e61ae29cec0b97f2ae0112794aa2a2220df9088
Len-77 <lennartschefe@web.de>
2025-01-26 11:07:55 +0100

    Debugging...

--- Volumen.py
+++ Volumen.py
@@ -4,7 +4,7 @@ from math import pi, log
 st.subheader("Volumenverdoppelungszeit")
 st.write("Ein Online-Minitool von Lennart Schefe")
 
-my_date = st.date_input("Datum der 1. Untersuchung:", value="today", format="DD/MM/YYYY")
+my_date = st.date_input("Datum der 1. Untersuchung:", value="today", format="DD.MM.YYYY")
 
 x = st.number_input("Durchmesser 1", key=0, step=0.1, format="%0.1f", min_value=0.0, value=1.0,
                     help="Gesamter Durchmesser, nicht Halbachse.")
@@ -12,18 +12,20 @@ y = st.number_input("Durchmesser 2", key=1, step=0.1, format="%0.1f", min_value=
                     help="Gesamter Durchmesser, nicht Halbachse.")
 z = st.number_input("Durchmesser 3", key=2, step=0.1, format="%0.1f", min_value=0.0, value=1.0,
                     help="Gesamter Durchmesser, nicht Halbachse.")
+
 volume = (4/3)*pi*(x/2)*(y/2)*(z/2)
 
 st.write(f"Volumen: {volume}, gerundet: {round(volume,3)}")
 
-my_date2 = st.date_input("Datum der 2. Untersuchung:", value="today", format="DD/MM/YYYY")
+my_date2 = st.date_input("Datum der 2. Untersuchung:", value="today", format="DD.MM.YYYY")
 
-x2 = st.number_input("Durchmesser 1", key=3, step=0.1, format="%0.1f", min_value=0.0, value=1.0,
+x2 = st.number_input("Durchmesser 1", key=3, step=0.1, format="%0.1f", min_value=0.0, value=2.0,
                     help="Gesamter Durchmesser, nicht Halbachse.")
-y2 = st.number_input("Durchmesser 2", key=4, step=0.1, format="%0.1f", min_value=0.0, value=1.0,
+y2 = st.number_input("Durchmesser 2", key=4, step=0.1, format="%0.1f", min_value=0.0, value=2.0,
                     help="Gesamter Durchmesser, nicht Halbachse.")
-z2 = st.number_input("Durchmesser 3", key=5, step=0.1, format="%0.1f", min_value=0.0, value=1.0,
+z2 = st.number_input("Durchmesser 3", key=5, step=0.1, format="%0.1f", min_value=0.0, value=2.0,
                     help="Gesamter Durchmesser, nicht Halbachse.")
+
 volume2 = (4/3)*pi*(x2/2)*(y2/2)*(z2/2)
 
 st.write(f"Volumen: {volume2}, gerundet: {round(volume2,3)}")
